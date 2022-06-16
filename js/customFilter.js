5190a5f037db91ebfd5d90503700b3a0143b9d15
NhanTruong0411 <truongnhan0411@gmail.com>
2022-06-16 10:49:00 +0700

    customFilter 2 options

--- js/customFilter.js
+++ js/customFilter.js
@@ -21,100 +21,96 @@ function customFilter(arr, callback) {
    // return arrayResult;
 }
 
-// myReset = () => {
-//    document.getElementById("myInput").value = "";
-// }
+myReset = () => {
+   document.getElementById("myInput").value = "";
+}
 
-// myFunction = () => {
-//    console.time("timer");
-//    let input = document.getElementById("myInput").value;
-//    let compareAction = document.getElementById('compareAction').value;
-//    let myCompare = Number(document.getElementById('myCompare').value);
-//    let messageText = document.getElementById('message-error');
-//    let messageCompare = document.getElementById('message-error-com');
-//    let resultDiv = document.getElementById('result');
-//    let resultString = "";
-//    // check input rỗng
-//    if (!input) {
-//       messageText.innerHTML = "This field is required !";
-//       messageText.style.visibility = "visible";
-//    } 
-//    // check comparison value rỗng   
-//    if (!myCompare) {
-//       messageCompare.innerHTML = "This field is required !";
-//       messageCompare.style.visibility = "visible";
-//    }
+myFunction = () => {
+   console.time("timer");
+   let input = document.getElementById("myInput").value;
+   let compareAction = document.getElementById('compareAction').value;
+   let myCompare = Number(document.getElementById('myCompare').value);
+   let messageText = document.getElementById('message-error');
+   let messageCompare = document.getElementById('message-error-com');
+   let resultDiv = document.getElementById('result');
+   let resultString = "";
+   // check input rỗng
+   if (!input) {
+      messageText.innerHTML = "This field is required !";
+      messageText.style.visibility = "visible";
+   } 
+   // check comparison value rỗng   
+   if (!myCompare) {
+      messageCompare.innerHTML = "This field is required !";
+      messageCompare.style.visibility = "visible";
+   }
    
-//    else {
-//       messageText.style.visibility = "hidden";
-//       messageCompare.style.visibility = "hidden";
-//       // check các giá trị trong input có phải là number ko?
-//       let arrayInput = input.split(',').map(Number);
-//       for (let i = 0; i < arrayInput.length; i++) {
-//          if (isNaN(arrayInput[i])) {
-//             messageText.innerHTML = "This field must be a NUMBER and separate by a comma !";
-//             messageText.style.visibility = "visible";
-//             break;
-//          } 
-//       }
-//       let result = customFilter(arrayInput,compareAction,myCompare);
-//       resultString = "Result : ";
-//       result.forEach(el => {
-//          resultString += `${el}, `
-//       })
-//       resultString = resultString.slice(0, resultString.length-2); 
-//       resultDiv.innerHTML = resultString;
-//    }
-//    console.timeEnd("timer");
-// }
+   else {
+      messageText.style.visibility = "hidden";
+      messageCompare.style.visibility = "hidden";
+      // check các giá trị trong input có phải là number ko?
+      let arrayInput = input.split(',').map(Number);
+      for (let i = 0; i < arrayInput.length; i++) {
+         if (isNaN(arrayInput[i])) {
+            messageText.innerHTML = "This field must be a NUMBER and separate by a comma !";
+            messageText.style.visibility = "visible";
+            break;
+         } 
+      }
+      let result = customFilter2(arrayInput,compareAction,myCompare);
+      resultString = "Result : ";
+      result.forEach(el => {
+         resultString += `${el}, `
+      })
+      resultString = resultString.slice(0, resultString.length-2); 
+      resultDiv.innerHTML = resultString;
+   }
+   console.timeEnd("timer");
+}
 
-// customFilter = (arrayInput, compareAction, myCompare) => {
-//    let result = [];
+customFilter2 = (arrayInput, compareAction, myCompare) => {
+   let result = [];
 
-//    arrayInput.forEach(element => {
-//       switch(compareAction) {
-//          case "=":
-//             if (element == myCompare) {
-//                result.push(element);
-//             }
-//             break;
-//          case ">":
-//             if (element > myCompare) {
-//                result.push(element);
-//             }
-//             break;
-//          case "<":
-//             if (element < myCompare) {
-//                result.push(element);
-//             }
-//             break;
-//          case ">=":
-//             if (element >= myCompare) {
-//                result.push(element);
-//             }
-//             break;
-//          case "<=":
-//             if (element <= myCompare) {
-//                result.push(element);
-//             }
-//             break;
-//          case "%":
-//             if (element % myCompare == 0) {
-//                result.push(element);
-//             }
-//             break;
-//          case "/":
-//             if (element % myCompare != 0) {
-//                result.push(element);
-//             }
-//             break;
-//       }
-//    });
-//    return result;
-// }
-
-// myFilter = () => {
-
-// }
+   arrayInput.forEach(element => {
+      switch(compareAction) {
+         case "=":
+            if (element == myCompare) {
+               result.push(element);
+            }
+            break;
+         case ">":
+            if (element > myCompare) {
+               result.push(element);
+            }
+            break;
+         case "<":
+            if (element < myCompare) {
+               result.push(element);
+            }
+            break;
+         case ">=":
+            if (element >= myCompare) {
+               result.push(element);
+            }
+            break;
+         case "<=":
+            if (element <= myCompare) {
+               result.push(element);
+            }
+            break;
+         case "%":
+            if (element % myCompare == 0) {
+               result.push(element);
+            }
+            break;
+         case "/":
+            if (element % myCompare != 0) {
+               result.push(element);
+            }
+            break;
+      }
+   });
+   return result;
+}
 
 
